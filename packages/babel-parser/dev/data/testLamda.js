57911dc90b03119637753f4060a7444f29e3b7f6
zouchengzhuo <405966530@qq.com>
2021-09-24 15:38:52 +0800

    提交相关文档

---
+++ packages/babel-parser/dev/data/testLamda.js
@@ -0,0 +1,4 @@
+const a = num => {
+  return 100 * num;
+};
+console.log(a(1.23));
--- packages/babel-parser/dev/test.js
+++ packages/babel-parser/dev/test.js
@@ -5,6 +5,10 @@ import { parse } from "../src";
 import fs from "fs";
 import path from "path";
 
+// var file = fs.readFileSync(
+//   path.resolve(__dirname, "./data/testLamda.js"),
+//   "utf8"
+// );
 var file = fs.readFileSync(path.resolve(__dirname, "./data/testFn.js"), "utf8");
 var ast = parse(file);
 
